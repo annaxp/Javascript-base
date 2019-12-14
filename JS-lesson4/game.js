59929fa9647f68ddc9476ab54b1f78621fce6859
annaxp <annaxp@ya.ru>
2019-12-15 00:11:30 +0300

    Lesson 4 part 3 game 10x10 'x,o'

---
+++ JS-lesson4/game.js
@@ -0,0 +1,89 @@
+'use strict';
+const settings = {
+    rowCount: 10,
+    colCount: 10,
+    startPositionX: 0,
+    startPositionY: 0,
+};
+
+const player = {
+    x: null,
+    y: null,
+
+    init(startX, startY) {
+        this.x = startX;
+        this.y = startY;
+    },
+
+    move(direction) {
+        switch (direction) {
+            case 2:
+                this.y++;
+                break;
+            case 4:
+                this.x--;
+                break;
+            case 6:
+                this.x++;
+                break;
+            case 8:
+                this.y--;
+                break;
+        }
+    },
+};
+
+const game = {
+    settings,
+    player,
+
+    run() {
+        this.player.init(this.settings.startPositionX, this.settings.startPositionY);
+
+        while (true) {
+            this.render();
+
+        const direction = this.getDirection();
+
+        if (direction === -1) {
+            return alert('До свидания!');
+        }
+
+        this.player.move(direction);
+        }
+    },
+
+    render() {
+        let map = '';
+        for (let row = 0; row < this.settings.rowCount; row++) {
+            for(let col = 0; col < this.settings.colCount; col++) {
+                if(this.player.y === row && this.player.x === col) {
+                    map += 'o ';
+                } else {
+                    map += 'x ';
+                }
+            }
+            map += '\n';
+        }
+
+        console.clear();
+        console.log(map);
+    },
+
+    getDirection() {
+        const availableDirection = [-1, 2, 4, 6, 8];
+
+        while (true) {
+            const direction = parseInt(prompt('Введите число, куда хотите переместиться или -1 для выхода'));
+
+            if (!availableDirection.includes(direction)) {
+                alert(`Для перемещения необходимо вывести одно из чисел: ${availableDirection.join(', ')}.`);
+                continue;
+            }
+
+            return direction;
+        }
+    },
+};
+
+game.run();
